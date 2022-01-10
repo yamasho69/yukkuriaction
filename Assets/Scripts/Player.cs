0bfb44c68ea8062e1c3fca6deddb641bacf0fd84
yamasho69 <nitiham55@yahoo.co.jp>
2022-01-11 00:34:25 +0900

    コミット01
    20210111初めてのコミット

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Player : MonoBehaviour{
+
+    private Animator anim = null;// アニメーターコンポーネントを入れる変数
+    
+    // Start is called before the first frame update
+    void Start(){
+        anim = GetComponent<Animator>();//アニメーターコンポーネントを取得、変数animに格納
+        Debug.Log("ok1");
+    }
+
+    // Update is called once per frame
+    void Update(){
+        float horizontalKey = Input.GetAxis("Horizontal");//左右方向のインプットを取得
+
+        if (horizontalKey > 0)//右方向の入力があった場合
+        {
+            Debug.Log("ok2");
+            anim.SetBool("right", true);
+            anim.SetBool("left", false);
+            //参考動画では画像を反転させて左右への移動を処理
+            //transform.localScale = new Vector3(1,1,1);
+            //anim.SetBool("run", true);
+        }
+        else if (horizontalKey < 0)//左方向の入力があった場合
+        {
+            anim.SetBool("right", false);
+            anim.SetBool("left", true);
+            //transform.localScale = new Vector3(-1,1,1);
+            //anim.SetBool("run", true);
+        }
+        else
+        {
+            anim.SetBool("right", false);
+            anim.SetBool("left", false);
+        }
+    }
+}
---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Title : MonoBehaviour{
+
+    private bool firstPush = false;//初めてのプッシュかどうか
+
+    public void PressStart()
+    {
+        Debug.Log("Press Start!");
+
+        if (!firstPush)//プッシュ済みではない場合
+        {
+            Debug.Log("Go Next Scene");
+            //ここに次のシーンへ行く命令を書く
+
+            firstPush = true;//一度押すとプッシュ済に
+        }
+    }
+}
