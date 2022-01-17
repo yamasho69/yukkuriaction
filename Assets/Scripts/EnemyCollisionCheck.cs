338318da80096c0fa7f845cc6622c6e1ff846e6e
yamasho69 <nitiham55@yahoo.co.jp>
2022-01-18 01:17:45 +0900

    5回目のコミット
    レッスン46まで終了

---
+++ Assets/Scripts/EnemyCollisionCheck.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public class EnemyCollisionCheck : MonoBehaviour{
+
+    [HideInInspector] public bool isOn = false;
+
+    private string groundTag = "Ground";
+    private string enemyTag = "Enemy";
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.tag == groundTag || collision.tag == enemyTag) {
+            isOn = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) {
+        if (collision.tag == groundTag || collision.tag == enemyTag) {
+            isOn = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Enemy_Zako1.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+//レッスン46で作成
+
+
+public class Enemy_Zako1 : MonoBehaviour {
+    #region//インスペクターで設定する
+    [Header("移動速度")] public float speed;
+    [Header("重力")] public float gravity;
+    [Header("画面外でも行動する")] public bool nonVisibleAct;
+    [Header("接触判定")] public EnemyCollisionCheck checkCollision;
+    #endregion
+
+    #region//プライベート変数
+    private Rigidbody2D rb = null;
+    private SpriteRenderer sr = null;
+    private Animator anim = null;
+    private ObjectCollision oc = null;
+    private CircleCollider2D col = null;
+    private bool rightTleftF = false;
+    private bool isDead = false;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
+        oc = GetComponent<ObjectCollision>();
+        col = GetComponent<CircleCollider2D>();
+    }
+
+    void FixedUpdate() {
+        if (!oc.playerStepOn) {
+            if (sr.isVisible || nonVisibleAct) {
+                if (checkCollision.isOn) {
+                    rightTleftF = !rightTleftF;
+                }
+                int xVector = -1;
+                if (rightTleftF) {
+                    xVector = 1;
+                    transform.localScale = new Vector3(-10, 10, 1);
+                } else {
+                    transform.localScale = new Vector3(10, 10, 1);
+                }
+                rb.velocity = new Vector2(xVector * speed, -gravity);
+            } else {
+                rb.Sleep();
+            }
+        } else {
+            if (!isDead) {
+                anim.Play("dead");
+                rb.velocity = new Vector2(0, -gravity);
+                isDead = true;
+                col.enabled = false;//CircleCollider2Dを無効にする。
+                Destroy(gameObject, 3f);
+            } else {
+                transform.Rotate(new Vector3(0, 0, 5));
+            }
+        }
+    }
+}
