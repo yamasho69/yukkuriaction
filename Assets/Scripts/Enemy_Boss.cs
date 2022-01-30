5a4af4ed798e7789ad7082c5d046246bf6a38c6a
yamasho69 <nitiham55@yahoo.co.jp>
2022-01-30 23:18:40 +0900

    16回目のコミット
    ステージ1はほぼ完成。タイトル画面、ラスボス、リトライボタン、ランダムボタン、残機アップを実装し、ステージ3まで作れば完成。

---
+++ Assets/Scripts/Enemy_Boss.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+
+//レッスン59で作成
+public class Enemy_Boss : MonoBehaviour{
+    
+    private enum BossState {
+        StartEnsyutu,
+        Battle,
+        ClearEnsyutu
+    }
+
+    private BossState nowState = BossState.StartEnsyutu;
+
+    // Start is called before the first frame update
+    void Start(){
+        
+    }
+
+    // Update is called once per frame
+    void Update(){
+        switch (nowState) {
+            case BossState.StartEnsyutu:
+                StartEnsyutu();
+                break;
+            case BossState.Battle:
+                break;
+            case BossState.ClearEnsyutu:
+                break;
+        }
+    }
+    private void StartEnsyutu() {
+        
+    }
+
+}
