9ea788ea00203232ce11fc8f3f194c3d17267102
yamasho69 <nitiham55@yahoo.co.jp>
2022-02-04 01:11:12 +0900

    21回目のコミット
    Dotweenを導入、ダンボールのアニメーションを追加

--- Assets/Scripts/ContinuePoint.cs
+++ Assets/Scripts/ContinuePoint.cs
@@ -5,13 +5,18 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using DG.Tweening;
+
 //レッスン56で作成
 public class ContinuePoint : MonoBehaviour {
     [Header("コンティニュー番号")] public int continueNum;
+    [Header("声")] public AudioClip voice;
     [Header("音")] public AudioClip se;
     [Header("プレイヤー判定")] public PlayerTriggerCheck trigger;
     [Header("スピード")] public float speed = 2.0f;
     [Header("取得アニメーション")] public AnimationCurve curve;
+    [Header("変更前ダンボール")]public Image beforeBox;
+    [Header("変更後ダンボール")]public Sprite afterBox;
 
     private bool on;
     private float timer;
@@ -29,19 +34,32 @@ public class ContinuePoint : MonoBehaviour {
     void Update() {
         if (trigger.isOn && !on) {
             GameManager.instance.continueNum = continueNum;
+            GameManager.instance.playSE(voice);
             GameManager.instance.playSE(se);
+            beforeBox.sprite = afterBox;
             on = true;
         }
         if (on) {
-            if(timer < 1.0f) {
-                transform.localScale = Vector3.one * curve.Evaluate(timer);
-                timer += speed * Time.deltaTime;
-
-            } else {
-                transform.localScale = Vector3.one * curve.Evaluate(1.0f);
-                gameObject.SetActive(false);
-                on = false;
-            }
+            //if(timer < 1.0f) {
+            //transform.localScale = Vector3.one * curve.Evaluate(timer);
+            //timer += speed * Time.deltaTime;
+           
+            Invoke("Rotate", 1.5f);
+            //} else {
+                //transform.localScale = Vector3.one * curve.Evaluate(1.0f);
+                //gameObject.SetActive(false);
+                //on = false;
+            //}
         }
+    }
+
+    void Rotate() {
+        this.transform.DORotate(Vector3.right * 90f, 1.5f);
+        Invoke("Vanish", 1.5f);
+    }
+
+    void Vanish(){
+        gameObject.SetActive(false);
+        on = false;
     }
 }
